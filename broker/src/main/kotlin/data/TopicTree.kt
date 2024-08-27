e547175c44f9775967f37b1e0adaa85520ff314a
Andreas Vogler <andreas.vogler@me.com>
2024-08-27 13:38:44 +0200

    Create TopicTree.kt

---
+++ broker/src/main/kotlin/data/TopicTree.kt
@@ -0,0 +1,20 @@
+package at.rocworks.data
+
+interface TopicTree {
+    fun add(topicName: MqttTopicName)
+    fun add(topicName: MqttTopicName, data: String?)
+    fun del(topicName: MqttTopicName)
+    fun del(topicName: MqttTopicName, data: String?)
+
+    /*
+       The given topicName will be matched with potential wildcard topics of the tree (tree contains wildcard topics)
+        */
+    fun findDataOfTopicName(topicName: MqttTopicName): List<String>
+
+    /*
+       The given topicName can contain wildcards and this will be matched with the tree topics without wildcards
+        */
+    fun findMatchingTopicNames(topicName: MqttTopicName): List<MqttTopicName>
+    fun findMatchingTopicNames(topicName: MqttTopicName, callback: (MqttTopicName)->Boolean)
+
+}
