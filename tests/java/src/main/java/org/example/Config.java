a4b7bfd1d9b254d381baaf4f8faba7409e8b7a39
Andreas Vogler <andreas.vogler@me.com>
2024-09-01 21:56:36 +0200

    Update Config.java

--- tests/java/src/main/java/org/example/Config.java
+++ tests/java/src/main/java/org/example/Config.java
@@ -3,8 +3,8 @@ package org.example;
 public class Config {
     public static String statBroker = "tcp://scada:1883";
 
-    public static String[] publisherBroker = new String[] { "tcp://nuc1:1883" };
-    public static String[] subscriberBroker = new String[] { "tcp://nuc1:1883" };
+    public static String[] publisherBroker = new String[] { "tcp://localhost:1883" };
+    public static String[] subscriberBroker = new String[] { "tcp://localhost:1883" };
 
     //public static String[] publisherBroker = new String[] { "tcp://linux1:1883", "tcp://linux2:1883", "tcp://linux3:1883" };
     //public static String[] subscriberBroker = new String[] { "tcp://linux1:1883", "tcp://linux2:1883", "tcp://linux3:1883" };
@@ -13,17 +13,17 @@ public class Config {
     public static String topicPrefix = "test";
     public static String statisticsTopic = "monitor"; // Topic for publishing statistics
 
-    public static int PUBLISHER_COUNT = 1000;
-    public static int SUBSCRIBER_COUNT = 1000;
+    public static int PUBLISHER_COUNT = 1;
+    public static int SUBSCRIBER_COUNT = 1;
 
     public static boolean SUBSCRIBER_CHECK_MESSAGE_ORDER = false;
 
-    public static double DELAY_PROCESSING_EVERY_100_MESSAGES = 0;
+    public static double DELAY_PROCESSING_EVERY_100_MESSAGES = 10;
     public static double DELAY_PROCESSING_EVERY_10_MESSAGES = 0;
-    public static double DELAY_PROCESSING_EVERY_MESSAGE = 50;
+    public static double DELAY_PROCESSING_EVERY_MESSAGE = 0;
 
-    public static int TOPIC_LEVEL_DEPTH = 0; //100;
-    public static boolean RETAINED_MESSAGES = false;
+    public static int TOPIC_LEVEL_DEPTH = 100; //100;
+    public static boolean RETAINED_MESSAGES = true;
     public static int QOS = 0;
 
 }
