d42590f1a0f770a73fa7f73a0fb98b2fc3b26f79
Andreas Vogler <andreas.vogler@me.com>
2024-09-08 14:16:14 +0200

    Deploy scripts update

--- broker/deploy-deps.bat
+++
@@ -1,6 +0,0 @@
-scp -r target\dependencies vogler@nuc1:Workspace/monster-mq/
-scp -r target\dependencies vogler@linux0:Workspace/monster-mq/
-scp -r target\dependencies vogler@linux1:Workspace/monster-mq/
-scp -r target\dependencies vogler@linux2:Workspace/monster-mq/
-scp -r target\dependencies vogler@linux3:Workspace/monster-mq/
-scp -r target\dependencies vogler@linux4:Workspace/monster-mq/
--- broker/deploy.bat
+++ broker/deploy.bat
@@ -1,3 +1,7 @@
 call mvn package
-call deploy-app.bat
-call deploy-deps.bat
+scp target/broker-1.0-SNAPSHOT-jar-with-dependencies.jar vogler@nuc1:Workspace/monster-mq/
+scp target/broker-1.0-SNAPSHOT-jar-with-dependencies.jar vogler@linux0:Workspace/monster-mq/
+scp target/broker-1.0-SNAPSHOT-jar-with-dependencies.jar vogler@linux1:Workspace/monster-mq/
+scp target/broker-1.0-SNAPSHOT-jar-with-dependencies.jar vogler@linux2:Workspace/monster-mq/
+scp target/broker-1.0-SNAPSHOT-jar-with-dependencies.jar vogler@linux3:Workspace/monster-mq/
+scp target/broker-1.0-SNAPSHOT-jar-with-dependencies.jar vogler@linux4:Workspace/monster-mq/
