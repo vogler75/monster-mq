d8db8106d602046841eb3a21cf65901f77d7878c
Andreas Vogler <andreas.vogler@me.com>
2024-09-28 11:43:45 +0200

    Keep datatype and handle STATE topic

--- broker/src/main/kotlin/extensions/SparkplugExtension.kt
+++ broker/src/main/kotlin/extensions/SparkplugExtension.kt
@@ -13,28 +13,33 @@ class SparkplugExtension(config: JsonObject) {
     private val sourceNamespace = "spBv1.0/"
     private val expandedNamespace= "spBv1.0e/"
 
+    private val messageTypes = listOf( // message types with protobuf data
+        "NBIRTH", // Birth certificate for Sparkplug Edge Nodes
+        "NDEATH", // Death certificate for Sparkplug Edge Nodes
+        "DBIRTH", // Birth certificate for Devices
+        "DDEATH", // Death certificate for Devices
+        "NDATA",  // Edge Node data message
+        "DDATA",  // Device data message
+        "NCMD",   // Edge Node command message
+        "DCMD",   // Device command message
+        "STATE"   // Sparkplug Host Application state message ==> STATE is not protobuf
+    )
+
     init {
         logger.level = Const.DEBUG_LEVEL
-        logger.info("Initialize SparkplugbHandler")
+        logger.info("Initialize Sparkplug Handler")
     }
 
     fun metricExpansion(message: MqttMessage, callback: (MqttMessage) -> Unit) {
         if (message.topicName.startsWith(sourceNamespace))
         try {
-            val spb = decoder.buildFromByteArray(message.payload, null)
-            logger.finest { "Received message [${spb}] [${Utils.getCurrentFunctionName()}]" }
-            spb.metrics.forEach { metric ->
-                val name = if (metric.hasName()) metric.name else "alias/${metric.alias}"
-                val topic = message.topicName.replaceFirst(sourceNamespace, expandedNamespace) + "/$name"
-                val payload = JsonObject()
-                    .put("Value", metric.value.toString())
-                    .put("Timestamp", metric.timestamp)
-                    .put("Quality", metric.alias)
-                    .put("DataType", metric.dataType.toString())
+            val levels = message.topicName.split("/") // spBv1.0/namespace/group_id/message_type/edge_node_id/[device_id]
+            if (levels.size>3 && levels[3] == "STATE") { // STATE is not a protobuf message
+                val topic = message.topicName.replaceFirst(sourceNamespace, expandedNamespace)
                 val mqttMessage = MqttMessage(
                     messageId = 0,
                     topicName = topic,
-                    payload = payload.encode().toByteArray(),
+                    payload = message.payload,
                     qosLevel = 0,
                     isRetain = false,
                     isDup = false,
@@ -42,6 +47,34 @@ class SparkplugExtension(config: JsonObject) {
                     clientId = message.clientId
                 )
                 callback(mqttMessage)
+            } else {
+                val spb = decoder.buildFromByteArray(message.payload, null)
+                logger.finest { "Received message [${spb}] [${Utils.getCurrentFunctionName()}]" }
+                spb.metrics.forEach { metric ->
+                    val name = if (metric.hasName()) metric.name else "alias/${metric.alias}"
+                    val topic = message.topicName.replaceFirst(sourceNamespace, expandedNamespace) + "/$name"
+                    val payload = JsonObject()
+                        .put("Timestamp", metric.timestamp)
+                        .put("DataType", metric.dataType.toString())
+                    try {
+                        payload.put("Value", metric.value)
+                    } catch (e: Exception) {
+                        logger.severe { "Failed to decode metric value [${name}]: [${e.message}] [${Utils.getCurrentFunctionName()}]" }
+                        payload.put("Value", null)
+                    }
+
+                    val mqttMessage = MqttMessage(
+                        messageId = 0,
+                        topicName = topic,
+                        payload = payload.encode().toByteArray(),
+                        qosLevel = 0,
+                        isRetain = false,
+                        isDup = false,
+                        isQueued = false,
+                        clientId = message.clientId
+                    )
+                    callback(mqttMessage)
+                }
             }
         } catch (e: Exception) {
             logger.severe { "Failed to decode message [${message.topicName}]: [${e.message}] [${Utils.getCurrentFunctionName()}]" }
