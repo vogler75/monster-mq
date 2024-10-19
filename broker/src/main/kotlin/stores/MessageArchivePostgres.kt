ebba07d9fdb54a1ffce1a44ccb842d3646bb7e45
Andreas Vogler <andreas.vogler@me.com>
2024-10-19 15:52:08 +0200

    Update MessageArchivePostgres.kt

--- broker/src/main/kotlin/stores/MessageArchivePostgres.kt
+++ broker/src/main/kotlin/stores/MessageArchivePostgres.kt
@@ -47,11 +47,12 @@ class MessageArchivePostgres (
 
                     // Check if TimescaleDB extension is available
                     val resultSet = statement.executeQuery("SELECT 1 FROM pg_extension WHERE extname = 'timescaledb';")
-                    if (resultSet.next()) {
+                    if (resultSet.first()) {
                         logger.info("TimescaleDB extension is available [${Utils.getCurrentFunctionName()}]")
                         val hypertableCheck =
                             statement.executeQuery("SELECT 1 FROM timescaledb_information.hypertables WHERE hypertable_name = '$tableName';")
-                        if (!hypertableCheck.next()) {
+                        if (!hypertableCheck.first()) {
+                            logger.info("Table $tableName convert to hypertable... [${Utils.getCurrentFunctionName()}]")
                             statement.executeUpdate("SELECT create_hypertable('$tableName', 'time');")
                             logger.info("Table $tableName converted to hypertable [${Utils.getCurrentFunctionName()}]")
                         } else {
