2eb0f780cd0752ad1e3b377ca63cb74c3754b172
Andreas Vogler <andreas.vogler@me.com>
2025-10-17 17:44:05 +0200

    Update sidebar.js

--- broker/src/main/resources/dashboard/js/sidebar.js
+++ broker/src/main/resources/dashboard/js/sidebar.js
@@ -58,7 +58,7 @@ class SidebarManager {
                     },
                     {
                         href: '/pages/jdbc-loggers.html',
-                        icon: '<path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline>',
+                        icon: '<ellipse cx="12" cy="5" rx="9" ry="3"></ellipse><path d="M21 12c0 1.66-4 3-9 3s-9-1.34-9-3"></path><path d="M3 5v14c0 1.66 4 3 9 3s9-1.34 9-3V5"></path>',
                         text: 'Loggers'
                     },
                     {
