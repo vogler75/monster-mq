74c2cbe0d054db411e7d751af4a10c7d4854240f
Andreas Vogler <andreas.vogler@me.com>
2026-03-31 10:47:14 +0200

    Update cluster configs, remove node4, update startup
    Refactor cluster node configuration and startup script:
    
    - Replace individual store type keys with DefaultStoreType: POSTGRES.
    - Disable UserManagement by default and remove StoreType lines.
    - Remove SQLite and Kafka sections and unused Metrics StoreType entries.
    - Enable GraphQL per node and assign ports 4001/4002/4003 respectively.
    - Update Postgres/CrateDB/MongoDB DB URLs to use the 'monster-cluster' database.
    - Delete config-cluster-node-4.yaml (node4 removed).
    - Update start_cluster.sh to point to configs/config-cluster-node-*.yaml, set log level to INFO, and stop launching node4.
    
    Note: node2 and node3 show 'axMessageSizeKb' (missing leading 'M') which looks like a typo and should be reviewed.

--- broker/start_cluster.sh
+++ broker/start_cluster.sh
@@ -1,6 +1,5 @@
 mkdir -p log
-./run.sh -cluster -config config1.yaml -log FINE > log/node1.out 2>&1 &
-./run.sh -cluster -config config2.yaml -log FINE > log/node2.out 2>&1 &
-./run.sh -cluster -config config3.yaml -log FINE > log/node3.out 2>&1 &
-./run.sh -cluster -config config4.yaml -log FINE > log/node4.out 2>&1 &
+./run.sh -cluster -config configs/config-cluster-node-1.yaml -log INFO > log/node1.out 2>&1 &
+./run.sh -cluster -config configs/config-cluster-node-2.yaml -log INFO > log/node2.out 2>&1 &
+./run.sh -cluster -config configs/config-cluster-node-3.yaml -log INFO > log/node3.out 2>&1 &
 
