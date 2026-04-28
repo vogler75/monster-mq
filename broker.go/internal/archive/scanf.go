59192a2733514d590dfc8fbe91eca007f8703490
Andreas Vogler <andreas.vogler@me.com>
2026-04-28 14:13:34 +0200

    Removed broker.go from this repo

--- broker.go/internal/archive/scanf.go
+++
@@ -1,7 +0,0 @@
-package archive
-
-import "fmt"
-
-func _sscan(s, format string, a ...any) (int, error) {
-	return fmt.Sscanf(s, format, a...)
-}
--- broker.go/internal/graphql/resolvers/password.go
+++
@@ -1,7 +0,0 @@
-package resolvers
-
-import "monstermq.io/edge/internal/stores/sqlite"
-
-func hashPassword(p string) (string, error) {
-	return sqlite.HashPassword(p)
-}
