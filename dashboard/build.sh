e78ac5fd8af68e81a5b65f3ff4f03fa59fa81e84
Andreas Vogler <andreas.vogler@me.com>
2026-05-01 21:38:40 +0200

    Remove dashboard from repo

--- dashboard/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Build dashboard and copy to broker classpath resources
-cd "$(dirname "$0")"
-npm ci
-npm run build
-rm -rf ../broker/src/main/resources/dashboard
-cp -r dist ../broker/src/main/resources/dashboard
-rm -f ../broker/src/main/resources/dashboard/config/brokers.json
-echo "Dashboard copied to broker/src/main/resources/dashboard"
