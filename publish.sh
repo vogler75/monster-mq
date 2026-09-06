fdffe38a4f48e44fbdfc50842684e63b08e5d73f
Andreas Vogler <andreas.vogler@me.com>
2026-09-06 09:00:41 +0200

    fix(publish): update docker/build reference to docker/build.sh

--- publish.sh
+++ publish.sh
@@ -231,7 +231,7 @@ fi
 # 2. Push Multi-Arch Docker Image to Docker Hub
 if [ "$PUBLISH_DOCKER" = true ]; then
     echo -e "${GREEN}[2/2] Publishing Multi-Arch Docker Images to Docker Hub...${NC}"
-    (cd docker && ./build -d -y)
+    (cd docker && ./build.sh -d -y)
     echo -e "${GREEN}✓ Docker Hub images published successfully!${NC}"
 fi
 
